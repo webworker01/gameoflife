fb894d22a8ce5d13d352a282eb0a14ab49b8e087
webworker01 <webworker@live.com>
2014-01-22 18:30:04 -0500

    * Better documentation for file level and variables * Added stub method for seeding the map

---
+++ library/webworker01/GameOfLife/Cell.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Game of Life in PHP
+ * @package webworker01\GameOfLife
+ * @author webworker@live.com
+ * @since PHP 5.4.x 1/22/14 2:27 PM
+ * @copyright Copyright © Owen Cole 2014
+ */
+
+namespace webworker01\GameOfLife;
+
+/**
+ * Class Cell
+ * Represents an individual cell in the game
+ * @package webworker01\GameOfLife
+ */
+class Cell
+{
+
+} 

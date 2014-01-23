f81d44646f754456e24725554dfc0cdf8fdcc777
webworker01 <webworker@live.com>
2014-01-22 21:40:44 -0500

    * Added some structure to cell class, each cell should know its own coordinates and state * Map class now holds a collection of its own cell objects * Created simple random seeding function * Reduced composer file to only include monolog as we will be integrating that first * Added very simple client for the module in index.php ** Map state is serialized and stored in $_SESSION

--- index.php
+++ index.php
@@ -1,2 +1,83 @@
 <?php
-phpinfo();
+/**
+ * Game of Life in PHP
+ *
+ * Initial client for the modules
+ *
+ * @package webworker01\GameOfLife
+ * @author webworker@live.com
+ * @since PHP 5.4.x 1/21/14 5:17 PM
+ * @copyright Copyright © Owen Cole 2014
+ */
+session_start();
+
+ini_set('display_errors', 'on');
+error_reporting (E_ALL ^ E_NOTICE);
+
+//Default autoload for my webworker01\GameOfLife modules
+spl_autoload_register(function($class) {
+    require_once getcwd().'\\library\\'.$class.'.php';
+});
+
+//Add composer autoloader
+require 'vendor/autoload.php';
+
+use webworker01\GameOfLife;
+
+//See if we have a map stored already for the session
+if (empty($_SESSION['map']) || $_GET['seed'] == 'true') {
+    $map = new webworker01\GameOfLife\Map(100, 35);
+
+    $map->seed();
+
+    $_SESSION['map'] = serialize($map);
+} else {
+    $map = unserialize($_SESSION['map']);
+
+    $map->tick();
+}
+
+//Temporary view
+?>
+<html>
+<head>
+    <style>
+        body {
+            background-color: #333;
+            font-size: 14px;
+            font-family: monospace, serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        #menu {
+            color: white;
+            position:absolute;
+            top:0;
+            right:0;
+        }
+
+        #menu a, #menu a:visited {
+            color: white;
+        }
+
+        #menu a:hover {
+            color: #779FAA;
+        }
+
+        #map {
+            margin: 30px auto;
+            background-color: #CCC;
+            border:1px solid black;
+            width:800px;
+        }
+    </style>
+</head>
+<body>
+    <div id="menu"><a href="./?seed=true">Seed</a> | <a href="./">Refresh</a></div>
+
+    <div id="map">
+        <?= $map; ?>
+    </div>
+</body>
+</html>
--- library/webworker01/GameOfLife/Map.php
+++ library/webworker01/GameOfLife/Map.php
@@ -22,13 +22,19 @@ class Map
      * The total number of points across in the map
      * @var int|int
      */
-    public $xSize;
+    protected $xSize;
 
     /**
      * The total number of points vertical in the map
      * @var int|int
      */
-    public $ySize;
+    protected $ySize;
+
+    /**
+     * Collection of all cells in the map
+     * @var Array
+     */
+    protected $cells;
 
     /**
      * When instantiating the map
@@ -36,7 +42,7 @@ class Map
      * @param int $x
      * @param int $y
      */
-    public function __construct(int $x, int $y)
+    public function __construct($x, $y)
     {
         $this->xSize = $x;
         $this->ySize = $y;
@@ -50,18 +56,16 @@ class Map
      * Any live cell with more than three live neighbours dies, as if by overcrowding.
      * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
      *
-     * @param Array $oldMap A two dimensional array holding the previous map to run life transitions against
-     * @return Array $newMap A two dimensional array holding the next iteration of the map
      */
-    public function step(Array $oldMap)
+    public function tick()
     {
-        foreach ($oldMap as $xCoordinates => $yCoordinateData) {
-            foreach ($yCoordinateData as $yCoordinate) {
-
+        for ($y = 0; $y < $this->ySize; $y++) {
+            for ($x = 0; $x < $this->xSize; $x++) {
+                //$this->cells[$x][$y];
             }
         }
 
-        return $newMap;
+        //return $newMap;
     }
 
     /**
@@ -69,8 +73,35 @@ class Map
      *
      * @param array $coordinates
      */
-    public function seed(Array $coordinates)
+    public function seed()
     {
+        for ($y = 0; $y < $this->ySize; $y++) {
+            for ($x = 0; $x < $this->xSize; $x++) {
+                $this->cells[$x][$y] = new Cell($x, $y, mt_rand(0, 1));
+            }
+        }
+    }
 
+    /**
+     * Simple string based html output of the current map
+     * @return string Contains the current map in string form
+     */
+    public function __toString()
+    {
+        $stringOutput = '';
+
+        for ($y = 0; $y < $this->ySize; $y++) {
+            for ($x = 0; $x < $this->xSize; $x++) {
+                if ($this->cells[$x][$y]->state == false) {
+                    $stringOutput .= '.';
+                } else {
+                    $stringOutput .= 'O';
+                }
+            }
+
+            $stringOutput .= '<br>';
+        }
+
+        return $stringOutput;
     }
 }
